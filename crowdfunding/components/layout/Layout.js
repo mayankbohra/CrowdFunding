2036da92835972058f74747ad2da6d5350dc7925
mayankbohra <bohramayank4u@gmail.com>
2023-10-14 19:14:20 +0530

    add theme toggle feature

--- crowdfunding/components/layout/Layout.js
+++ crowdfunding/components/layout/Layout.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import { useState, createContext } from 'react';
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 
 import Header from './Header';
+import themes from './themes';
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const changeTheme = () => {
+    setTheme(theme == "light" ? "dark" : "light");
+  };
+
   return (
-    <div>
-      <Header />
-      {children}
-    </div>
+    <ThemeProvider theme={themes[theme]}>
+      <LayoutWrapper onClick={changeTheme}>
+        <GlobalStyle />
+        <Header />
+        {children}
+      </LayoutWrapper>
+    </ThemeProvider>
   )
 }
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        overflow-x: hidden;
+    }
+`;
+
+const LayoutWrapper = styled.div`
+  min-height: 100vh;
+  background-color: ${(props) => props.theme.bgColor};
+  background-image: ${(props) => props.theme.bgImage};
+  color: ${(props) => props.theme.color};
+`;
 
 export default Layout
