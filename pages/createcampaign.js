8c7ea7672416eba4282302d824dc09b51e779e0b
mayankbohra <bohramayank4u@gmail.com>
2023-10-16 05:26:45 +0530

    shift folders

---
+++ pages/createcampaign.js
@@ -0,0 +1,11 @@
+import Form from '../components/form/Form';
+
+const createcampaign = () => {
+  return (
+    <div suppressHydrationWarning={true}>
+      <Form/>
+    </div>
+  )
+}
+
+export default createcampaign
