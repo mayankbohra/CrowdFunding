3af7b1cc58acaa68ac3c042e9d3b0bb40bf72f5e
mayankbohra <bohramayank4u@gmail.com>
2023-10-15 21:32:02 +0530

    styled campaign card

--- crowdfunding/next.config.js
+++ crowdfunding/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains:  ['ipfs.infura.io', 'th.bing.com', 'crowdfunding.infura-ipfs.io']
+     }
+  }
+  
+  module.exports = nextConfig
