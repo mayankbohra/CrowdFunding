cec3390daf87de366593339548b3efee8c87370d
mayankbohra <bohramayank4u@gmail.com>
2023-10-16 05:30:01 +0530

    readme file added

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains:  ['ipfs.infura.io', 'th.bing.com', 'crowdfundingproject.infura-ipfs.io']
-     }
-  }
-  
-  module.exports = nextConfig
